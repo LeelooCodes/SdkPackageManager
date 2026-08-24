4a88a362fe1217ca1e8c3b2308064b89bf74788c
LeelooCodes <leeannmiller1909@gmail.com>
2026-08-24 21:55:56 +0100

    test: added native interop integration test.

---
+++ SdkPackageManager.Tests/NativeInteropIntegrationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SdkPackageManager.App.Interop;
+using Xunit;
+
+namespace SdkPackageManager.Tests;
+
+public class NativeInteropIntegrationTests
+{
+    [Fact]
+    [Trait("Category", "Integration")]
+    public void CompareVersions_ThroughNativeDll_ReturnsOlderForOlderVersion()
+    {
+        //Arrange
+        IPackageInterop interop = new NativePackageInterop();
+
+        //Act
+        int result = interop.CompareVersions(
+            1, 2, 0,
+            1, 4, 0);
+
+        //Assert
+        Assert.Equal(-1, result);
+    }
+}
